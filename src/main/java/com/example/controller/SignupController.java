a172eb72b5f506549e0a5b373538a29e7287839d
oikawayuki <oikawayuki99@gmail.com>
2023-05-07 03:34:27 +0900

    追加：バインド

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -2,20 +2,23 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.SignupForm;
 
 @Controller
 @RequestMapping("/user")
 public class SignupController {
   
   @GetMapping("/signup")
-  public String getSignup() {
+  public String getSignup(@ModelAttribute SignupForm form) {
     return "user/signup"; 
   }
   
   @PostMapping("/signup")
-  public String postSignup() {
+  public String postSignup(@ModelAttribute SignupForm form) {
     return "redirect:/login";
   }
 }
