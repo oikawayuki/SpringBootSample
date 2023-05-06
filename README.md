0e9c367af6405ef31ef982d5ac80c3e0a69f591e
oikawayuki <oikawayuki99@gmail.com>
2023-05-07 02:33:43 +0900

    追加：データベース設定

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SpringBootSample
+Spring Boot の勉強
+本の内容をもとにした
+mysql のデータベース
+- データベース名：testdb
+- ユーザー名：springuser
+- パスワード：password
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table if not exists m_user(
+  user_id varchar(50) primary key,
+  password varchar(100),
+  user_name varchar(50),
+  birthday date,
+  age int
+);
