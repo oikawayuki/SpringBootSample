f0b7ddb33c5e910dca0818ebf08d3930c56d7b2f
oikawayuki <oikawayuki99@gmail.com>
2023-05-07 04:07:34 +0900

    追加：バリデーション

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -1,11 +1,14 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 @Controller
@@ -18,7 +21,13 @@ public class SignupController {
   }
   
   @PostMapping("/signup")
-  public String postSignup(@ModelAttribute SignupForm form) {
+  public String postSignup(
+    @ModelAttribute @Validated(GroupOrder.class) SignupForm form,
+    BindingResult bindingResult) {
+    
+    if (bindingResult.hasErrors()) {
+      return getSignup(form);
+    }
     return "redirect:/login";
   }
 }
